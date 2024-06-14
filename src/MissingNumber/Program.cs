a315a0ac126f880d68fa96fefff405b1006e4113
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-06-15 05:50:30 +0600

    New file added

---
+++ src/MissingNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MissingNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] numbers = Console.ReadLine().Split();
+            int[] array = Array.ConvertAll(numbers, int.Parse);
+
+            int totalSum = n * (n + 1) / 2;
+            int sum = 0;
+
+            foreach (int num in array)
+            {
+                sum += num;
+
+            }
+
+            int missingSum = totalSum - sum;
+            Console.WriteLine(missingSum);
+        }
+    }
+}
